40add6e9f44760ef848ee41674672f4eb2eb876e
Peyton Bodhi <bodhipeyton@cityuniversity.edu>
2020-04-23 00:13:37 -0700

    Module 2 HelloForm Module 2 HelloWPF

---
+++ ON/HelloForm/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace HelloForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Button click event
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //Show message in label
+            label1.Text = "Hello, I am a CS student!";
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
