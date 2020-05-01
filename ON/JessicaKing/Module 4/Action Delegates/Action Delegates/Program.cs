56fa4fe6e31a8089e9a1fbd51f5812eb44c6b925
Jessica King <jessica429ballard@gmail.com>
2020-04-30 20:53:33 -0700

    HOP4- action delegates

---
+++ ON/JessicaKing/Module 4/Action Delegates/Action Delegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Action_Delegates
+{
+    class Program
+    {
+        public static void myfun(int p, int q)
+        {
+            Console.WriteLine(p - q);
+        }
+
+        //Main method
+        static public void Main()
+        {
+
+            //using action delegate
+            //here, action delegate
+            //contains two input parameters
+            Action<int, int> val = myfun;
+            val(20, 5);
+        }
+    }
+}
