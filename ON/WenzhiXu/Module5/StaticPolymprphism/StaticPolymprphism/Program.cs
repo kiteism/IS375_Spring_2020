36f5191e645ad8b66ced70298daccd4c385e63b5
Wenzhi <xuwenzhi7@gmail.com>
2020-05-05 16:25:12 -0700

    Add XuWenzhi Module5.

---
+++ ON/WenzhiXu/Module5/StaticPolymprphism/StaticPolymprphism/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StaticPolymprphism
+{
+    class Printdata
+    {
+        void print(int i)
+        {
+            Console.WriteLine("Printing int: {0}", i);
+        }
+
+        void print(double f)
+        {
+            Console.WriteLine("Printing float: {0}", f);
+        }
+
+        void print(string s)
+        {
+            Console.WriteLine("Printing string: {0}", s);
+        }
+
+        public static void Main(string[] args)
+        {
+            Printdata p = new Printdata();
+
+            p.print(5);
+
+            p.print(500.263);
+
+            p.print("Hello C++");
+            Console.ReadKey();
+        }
+    }
+}
