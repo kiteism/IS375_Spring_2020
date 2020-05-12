9cdb0b60bac9537bb8b2b8311548a119a9766bed
Wenzhi <xuwenzhi7@gmail.com>
2020-05-12 14:57:21 -0700

    Add XuWenzhi Module6.

---
+++ ON/WenzhiXu/Module6/ReadingFiles/ReadingFiles/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace ReadingFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader("test.txt");
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
