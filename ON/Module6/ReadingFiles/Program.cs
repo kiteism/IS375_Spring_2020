952e3b4808e0d8c855047200f1e5a3ebe4093d5d
Peyton Bodhi <bodhipeyton@cityuniversity.edu>
2020-05-13 23:26:49 -0700

    Module 6: DivideByZero ReadingFiles UserDefinedExceptions WritingFiles

---
+++ ON/Module6/ReadingFiles/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace ReadingFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader("C:/Users/Peyto/source/repos/Peytonbodhi/IS375_Spring_2020/ON/Module6/jamaica.txt"))
+                {
+                    string line;
+
+                    // Read and display lines from the file until
+                    // the end of the file is reached.
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know that went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
