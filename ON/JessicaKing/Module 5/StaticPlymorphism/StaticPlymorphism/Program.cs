0f4fbf850d8d179bce7d67827c2378e368d79995
Jessica King <jessica429ballard@gmail.com>
2020-05-22 14:41:29 -0700

    HOP5

---
+++ ON/JessicaKing/Module 5/StaticPlymorphism/StaticPlymorphism/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace StaticPlymorphism
+{
+    class Printdata
+    {
+        void print(int i)
+        {
+            Console.WriteLine("Printing int: {0}", i);
+        }
+
+        void print(double f)
+        {
+            Console.WriteLine("Printing float: {0}", f);
+        }
+
+        void print(string s)
+        {
+            Console.WriteLine("Printing string: {0}", s);
+        }
+
+        static void Main(string[] args)
+        {
+            Printdata p = new Printdata();
+
+            //call print to print integer
+            p.print(5);
+
+            //call print to print float
+            p.print(500.263);
+
+            //call print to print string
+            p.print("Hello C++");
+            Console.ReadKey();
+        }
+    }
+}
