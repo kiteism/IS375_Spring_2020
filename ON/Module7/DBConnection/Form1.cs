e1e64919bced8b22edd4af0ebd4ee103a699dc37
Peyton Bodhi <bodhipeyton@cityuniversity.edu>
2020-05-30 23:40:04 -0700

    Module 7 HOP - Connecting to Databases

---
+++ ON/Module7/DBConnection/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace DBConnection
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection dbConn;
+
+        String ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;" + "Data Source=C:/Users/Peyto/source/repos/IS375_Spring_2020/ON/PeytonBodhi/Module7/StudentsDB.accdb;" +
+            "Persist Security Info=False";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dbConn = new OleDbConnection(ConnectionString);
+                dbConn.Open();
+
+                MessageBox.Show("Connection open Successfully");
+
+                dbConn.Close();
+            }
+            catch (Exception EX) { MessageBox.Show(EX.Message); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dbConn = new OleDbConnection(ConnectionString);
+            dbConn.Open();
+
+            String sql;
+            sql = "select * From StudentsData;";
+
+            OleDbCommand dbCmd = new OleDbCommand(sql, dbConn);
+            OleDbDataReader rd = dbCmd.ExecuteReader();
+
+            DataTable dt = new DataTable();
+            dt.Load(rd);
+
+            dbConn.Close();
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
