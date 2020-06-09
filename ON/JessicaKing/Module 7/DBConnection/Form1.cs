5b8b48aea32b9cbad604e5c6b3598edb45ab033b
Jessica King <jessica429ballard@gmail.com>
2020-06-09 11:58:53 -0700

    HOP8

--- ON/JessicaKing/Module 7/DBConnection/Form1.cs
+++ ON/JessicaKing/Module 7/DBConnection/Form1.cs
@@ -55,5 +55,86 @@ namespace DBConnection
             dbConn.Close();
             dataGridView1.DataSource = dt;
         }
+
+        private void button3_Click(object sender, EventArgs e)
+            {
+        dbConn = new OleDbConnection(ConnectionString);
+            dbConn.Open();
+
+            OleDbCommand command;
+            OleDbDataAdapter adapter = new OleDbDataAdapter();
+            String sql ="";
+
+            sql = "insert into StudentsData (StudentID, StudentName, CourseCode, TeacherName) values (11, 'Eric Forman', 'IS375', 'Red Forman')";
+
+            command = new OleDbCommand(sql, dbConn);
+            adapter.InsertCommand = command;
+            adapter.InsertCommand.ExcecuteNonQuery();
+
+            MessageBox.Show("Added successfully");
+
+            command.Dipose();
+            dbConn.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+            {
+        dbConn = new OleDbConnection(ConnectionString);
+            dbConn.Open();
+
+            OleDbCommand command;
+            OleDbDataAdapter adapter = new OleDbDataAdapter();
+            String sql = "";
+
+            sql = "delete from StudentsData where StudentName= 'Eric Forman'";
+
+            command = new OleDbCommand(sql, dbConn);
+            adapter.DeleteCommand = command;
+            adapter.DeleteCommand.Connection.ExcecuteNonQuery();
+
+            MessageBox.Show("Deleted successfully");
+
+            command.Dispose();
+            dbConn.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+            {
+        dbConn - new OleDbConnection(ConnectionString);
+            dbConn.Open();
+
+            OleDbCommand command;
+            OleDbDataAdapter adapter = new OleDbDataAdapter();
+            String sql = "";
+
+            sql = "Update StudentsData set StudentName= 'Michael Kelso' where StudentID=4";
+
+            command = new OleDbCommand(sql, dbConn);
+            adapter.UpdateCommand = command;
+            adapter.UpdateCommand.ExcecuteNonQuery();
+
+            MessageBox.Show("Record updated successfully");
+
+            command.Dispose();
+            dbConn.Close();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+            {
+        dbConn = new OleDbConnection(ConnectionString);
+            dbConn.Open();
+
+            String sql;
+            sql = "Select * From StudentData where StudentName= 'Bill Mosby';";
+
+            OleDbCommand dbCmd = new OleDbCommand(sql, dbConn);
+            OleDbDataReader rd = dbCmd.ExcecuteReader();
+
+            DataTable dt = new DataTable();
+            dt.Load(rd);
+
+            dbConn.Close();
+            dataGridView1.DataSource = dt;
+        }
     }
 }
